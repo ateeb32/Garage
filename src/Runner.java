97c49989d085d773b03c85c3016c9e140db4ef54
ateeb32 <l.ateeb@hotmail.com>
2018-08-23 16:29:42 +0100

    garage mod, took out comment line

--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		Boat B1 = new Boat("3000", "Pink", 2);
-		Car C1 = new Car("6300", "White", 6);			// 
+		Car C1 = new Car("6300", "White", 6);
 		Plane P1 = new Plane ("15000", "Green", 2);
 		
 		Garage myGarage = new Garage();
